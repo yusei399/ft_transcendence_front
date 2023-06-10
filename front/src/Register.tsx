4e687b0e315e681f3dddfc395b4ad014a1163478
yusei399 <yusei2021@keio.jp>
2023-06-10 22:50:26 +0900

    wip

--- front/src/Register.tsx
+++ front/src/Register.tsx
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 import {
-  Input,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Button,
-  Box
-} from "@chakra-ui/react";
+	Input,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Button,
+	Box
+	} from "@chakra-ui/react";
 import axios from "axios";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [isSubmitted, setIsSubmitted] = useState(false);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
+		alert("Passwords do not match");
+		return;
     }
 
     try {
-      const res = await axios.post("/users/register", {
+		const res = await axios.post("/users/register", {
         username,
         password,
         confirmPassword
-      });
-      console.log(res.data);
+		});
+		console.log(res.data);
     } catch (err) {
 		console.error(err);
     }
@@ -38,69 +38,69 @@ const Register = () => {
     setPassword("");
     setConfirmPassword("");
 	setIsSubmitted(true);
-  };
+	};
 
-  const isUsernameEmpty = username.trim() === "";
-  const isPasswordEmpty = password.trim() === "";
-  const isConfirmPasswordEmpty = confirmPassword.trim() === "";
+	const isUsernameEmpty = username.trim() === "";
+	const isPasswordEmpty = password.trim() === "";
+	const isConfirmPasswordEmpty = confirmPassword.trim() === "";
 
-  return (
+	return (
     <>
-      <form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit}>
         <FormControl isRequired isInvalid={isUsernameEmpty}>
-          <FormLabel>Username</FormLabel>
-          <Input
-            placeholder="username"
-            variant="outline"
-            sx={{ borderWidth: "1px", borderColor: isUsernameEmpty ? "red.300" : "blue.500" }}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          {isUsernameEmpty && (
-            <FormErrorMessage color="red">Username is required.</FormErrorMessage>
-          )}
+			<FormLabel>Username</FormLabel>
+				<Input
+				placeholder="username"
+				variant="outline"
+				sx={{ borderWidth: "1px", borderColor: isUsernameEmpty ? "red.300" : "blue.500" }}
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				/>
+			{isUsernameEmpty && (
+				<FormErrorMessage color="red">Username is required.</FormErrorMessage>
+			)}
         </FormControl>
 
         <FormControl isRequired isInvalid={isPasswordEmpty}>
-          <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            placeholder="password"
-            variant="outline"
-            sx={{ borderWidth: "1px", borderColor: isPasswordEmpty ? "red.300" : "blue.500" }}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {isPasswordEmpty && (
-            <FormErrorMessage color="red">Password is required.</FormErrorMessage>
-          )}
+			<FormLabel>Password</FormLabel>
+			<Input
+				type="password"
+				placeholder="password"
+				variant="outline"
+				sx={{ borderWidth: "1px", borderColor: isPasswordEmpty ? "red.300" : "blue.500" }}
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			{isPasswordEmpty && (
+				<FormErrorMessage color="red">Password is required.</FormErrorMessage>
+			)}
         </FormControl>
 
         <FormControl isRequired isInvalid={isConfirmPasswordEmpty}>
-          <FormLabel>Confirm Password</FormLabel>
-          <Input
-            type="password"
-            placeholder="confirm password"
-            variant="outline"
-            sx={{ borderWidth: "1px", borderColor: isConfirmPasswordEmpty ? "red.300" : "blue.500" }}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {isConfirmPasswordEmpty && (
+			<FormLabel>Confirm Password</FormLabel>
+			<Input
+				type="password"
+				placeholder="confirm password"
+				variant="outline"
+				sx={{ borderWidth: "1px", borderColor: isConfirmPasswordEmpty ? "red.300" : "blue.500" }}
+				value={confirmPassword}
+				onChange={(e) => setConfirmPassword(e.target.value)}
+			/>
+			{isConfirmPasswordEmpty && (
             <FormErrorMessage color="red">Confirm Password is required.</FormErrorMessage>
-          )}
+			)}
         </FormControl>
 
         <Button type="submit">Submit</Button>
-      </form>
+		</form>
 
-      {isSubmitted && (
-        <Box mt={4} color="green">Registration successful! You can now log in.</Box>
-      )}
+		{isSubmitted && (
+			<Box mt={4} color="green">Registration successful! You can now log in.</Box>
+		)}
 
-      <h1>#Register Page</h1>
+		<h1>#Register Page</h1>
     </>
-  );
+	);
 };
 
 export default Register;
