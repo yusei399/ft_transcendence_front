2398402e4f2e3780c5598a58d7d8bb5002fd1a60
yusei399 <yusei2021@keio.jp>
2023-06-10 18:13:11 +0900

    登録form修正

--- front/src/Register.tsx
+++ front/src/Register.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Input,
   FormControl,
   FormLabel,
-  FormHelperText,
-  FormErrorMessage
+  FormErrorMessage,
+  Button,
+  Box
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useState } from "react";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -30,8 +31,13 @@ const Register = () => {
       });
       console.log(res.data);
     } catch (err) {
-      console.error(err);
+		console.error(err);
     }
+	
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+	setIsSubmitted(true);
   };
 
   const isUsernameEmpty = username.trim() === "";
@@ -40,54 +46,57 @@ const Register = () => {
 
   return (
     <>
-      <FormControl isRequired isInvalid={isUsernameEmpty}>
-        <FormLabel>Username</FormLabel>
-        <Input
-          placeholder="username"
-          variant="outline"
-          sx={{ borderWidth: "1px", borderColor: isUsernameEmpty ? "red.300" : "blue.500" }}
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        {isUsernameEmpty && (
-          <FormErrorMessage color="red">Username is required.</FormErrorMessage>
-        )}
-        {!isUsernameEmpty && (
-          <FormHelperText>
-            Enter the username you'd like to receive the newsletter on.
-          </FormHelperText>
-        )}
-      </FormControl>
+      <form onSubmit={handleSubmit}>
+        <FormControl isRequired isInvalid={isUsernameEmpty}>
+          <FormLabel>Username</FormLabel>
+          <Input
+            placeholder="username"
+            variant="outline"
+            sx={{ borderWidth: "1px", borderColor: isUsernameEmpty ? "red.300" : "blue.500" }}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {isUsernameEmpty && (
+            <FormErrorMessage color="red">Username is required.</FormErrorMessage>
+          )}
+        </FormControl>
 
-      <FormControl isRequired isInvalid={isPasswordEmpty}>
-        <FormLabel>Password</FormLabel>
-        <Input
-          type="password"
-          placeholder="password"
-          variant="outline"
-          sx={{ borderWidth: "1px", borderColor: isPasswordEmpty ? "red.300" : "blue.500" }}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {isPasswordEmpty && (
-          <FormErrorMessage color="red">Password is required.</FormErrorMessage>
-        )}
-      </FormControl>
+        <FormControl isRequired isInvalid={isPasswordEmpty}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="password"
+            variant="outline"
+            sx={{ borderWidth: "1px", borderColor: isPasswordEmpty ? "red.300" : "blue.500" }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {isPasswordEmpty && (
+            <FormErrorMessage color="red">Password is required.</FormErrorMessage>
+          )}
+        </FormControl>
 
-      <FormControl isRequired isInvalid={isConfirmPasswordEmpty}>
-        <FormLabel>Confirm Password</FormLabel>
-        <Input
-          type="password"
-          placeholder="confirm password"
-          variant="outline"
-          sx={{ borderWidth: "1px", borderColor: isConfirmPasswordEmpty ? "red.300" : "blue.500" }}
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        {isConfirmPasswordEmpty && (
-          <FormErrorMessage color="red">Confirm Password is required.</FormErrorMessage>
-        )}
-      </FormControl>
+        <FormControl isRequired isInvalid={isConfirmPasswordEmpty}>
+          <FormLabel>Confirm Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="confirm password"
+            variant="outline"
+            sx={{ borderWidth: "1px", borderColor: isConfirmPasswordEmpty ? "red.300" : "blue.500" }}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {isConfirmPasswordEmpty && (
+            <FormErrorMessage color="red">Confirm Password is required.</FormErrorMessage>
+          )}
+        </FormControl>
+
+        <Button type="submit">Submit</Button>
+      </form>
+
+      {isSubmitted && (
+        <Box mt={4} color="green">Registration successful! You can now log in.</Box>
+      )}
 
       <h1>#Register Page</h1>
     </>
@@ -95,6 +104,9 @@ const Register = () => {
 };
 
 export default Register;
+
+
+
 
 
 
