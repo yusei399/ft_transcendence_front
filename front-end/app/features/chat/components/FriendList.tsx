97dd199c4560fd8c57976cc9fb339a60f6bf77b8
yusei399 <yusei2021@keio.jp>
2023-10-28 11:50:52 +0900

    　fix directory

--- front-end/app/features/chat/components/FriendList.tsx
+++ front-end/app/features/chat/components/FriendList.tsx
@@ -1,6 +1,4 @@
 
-
-
 const FriendList = (prop : any) => {
 	return (
 		<>
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,23 +1,21 @@
-import LoginPage  from './components/auth/LoginPage'
-import UserEditProfile from './components/user/UserEditProfile';
-import UserProfile from './components/user/UserProfile';
+import LoginPage  from '@/app/features/auth/components/LoginPage';
 import { ChakraProvider } from "@chakra-ui/react";
+import UserProfile from './features/user/components/UserProfile';
 
 export default function main() {
 
-  const User = [{
-    name: "test",
-    email: "test.eamil.com",
-  },
-  {
-    name: "test2",
-    email: "test2.eamil.com",
-  }]
+  // const User = [{
+  //   name: "test",
+  //   email: "test.eamil.com",
+  // },
+  // {
+  //   name: "test2",
+  //   email: "test2.eamil.com",
+  // }]
   return (
     <>
       <ChakraProvider>
-        {/* <LoginPage /> */}
-        <UserEditProfile/>
+        <LoginPage />
         {/* <UserProfile /> */}
       </ChakraProvider>
     </>
