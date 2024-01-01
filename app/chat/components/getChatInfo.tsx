101d84aca2ad318e18a942769fc2920101ca081f
yusei399 <yusei2021@keio.jp>
2024-01-01 15:32:22 +0900

    コンポーネント追加

--- app/chat/components/getChatInfo.tsx
+++ app/chat/components/getChatInfo.tsx
@@ -1,10 +1,29 @@
+import Loading from '@/app/components/global/Loading';
 import { useGetChatInfoQuery } from '@/lib/redux/api';
 
-const useGetChatInfo = ({chatId}: {chatId: number}) => {
-	const queryResult = useGetChatInfoQuery([chatId]);
-	console.log(queryResult);
+const GetChatInfo = ({chatId}: {chatId: number}) => {
+  const { data, isLoading, error } = useGetChatInfoQuery([chatId]);
 
-	return queryResult;
+  if (isLoading) return <Loading />;
+  if (!data) return <div>no data</div>;
+  if (error) return console.log(error);
+
+  return (
+    <div>
+      <h2>Chat Information:</h2>
+      <p>Chat ID: {data?.chatId}</p>
+      <p>Name: {data?.name}</p>
+      <p>Has Password: {data?.hasPassword}</p>
+      <div>
+        Participants:
+        <ul>
+          {data?.participants?.map((participant, index) => (
+            <li key={index}>{participant.userProfile.nickname}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default useGetChatInfo;
+export default GetChatInfo;
